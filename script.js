5533c6c29990e58ad3e0b585f6ae121f1f163441
Julian Beldotti <julian.beldotti@gmail.com>
2023-12-28 00:00:42 -0700

    Feat: create dom

---
+++ script.js
@@ -0,0 +1,10 @@
+// To access an HTML element with a given id name, you can use the getElementById() method.
+
+const calorieCounter = document.getElementById("calorie-counter");
+const budgetInput = document.getElementById("budget");
+const entryDropdown = document.getElementById("entry-dropdown");
+const addEntryBtn = document.getElementById("add-entry");
+const clearButton = document.getElementById("clear");
+const output = document.getElementById("output");
+
+let isError = false;
