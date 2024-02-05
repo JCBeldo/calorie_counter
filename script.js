677915d4933cd3169e63d54f63e60d71dbbac843
Julian Beldotti <julian.beldotti@gmail.com>
2024-02-05 10:17:55 -0700

    Feat: add for attribute to the label tag

--- script.js
+++ script.js
@@ -32,6 +32,6 @@ function addEntry() {
   const targetId = '#' + entryDropdown.value;
   const targetInputContainer = document.querySelector(`${targetId} .input-container`);
   const entryNumber = targetInputContainer.querySelectorAll('input[type="text"]').length;
-  const HTMLString = `<label>Entry ${entryNumber} Name</label>`
+  const HTMLString = `<label for="${targetId}-${entryNumber}-name">Entry ${entryNumber} Name</label>`
 
 }
