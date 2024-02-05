7144a9013f1bf0f58a67ac2f067a59b50daa3ea6
Julian Beldotti <julian.beldotti@gmail.com>
2024-02-05 10:10:21 -0700

    Feat: get back the length of the char count for dropdown selector

--- script.js
+++ script.js
@@ -31,6 +31,6 @@ function isInvalidInput(str) {
 function addEntry() {
   const targetId = '#' + entryDropdown.value;
   const targetInputContainer = document.querySelector(`${targetId} .input-container`);
-  const entryNumber = targetInputContainer.querySelectorAll();
+  const entryNumber = targetInputContainer.querySelectorAll('input[type="text"]').length;
 
 }
