a1e02fef1db165b82acf2eb6aa14c09f7148ee72
Julian Beldotti <julian.beldotti@gmail.com>
2024-02-09 15:39:29 -0700

    Feat: add alert for invalid entries

--- script.js
+++ script.js
@@ -44,7 +44,11 @@ function addEntry() {
 function getCaloriesFromInputs(list) {
   let calories = 0;
   for (const item of list) {
-    const currVal = item.value;
+    const currVal = cleanInputString(item.value);
+    const invalidInputMatch = isInvalidInput(currVal);
+    if (invalidInputMatch) {
+      alert(`Invalid Input: ${invalidInputMatch[0]}`)
+    }
   }
 
 }
