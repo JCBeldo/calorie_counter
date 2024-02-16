81505ca0420ec6f3ca16f046f7f978a13fa30f34
Julian Beldotti <julian.beldotti@gmail.com>
2024-02-15 20:17:10 -0700

    Feat: Adjust output to display new tags and unhide the block

--- script.js
+++ script.js
@@ -78,4 +78,13 @@ function calculateCalories(e) {
 
   const consumedCalories = breakfastCalories + lunchCalories + dinnerCalories + snacksCalories;
   const remainingCalories = budgetCalories - consumedCalories + exerciseCalories;
+  const surplusOrDeficit = remainingCalories < 0 ? 'Surplus' : 'Deficit';
+
+  output.innerHTML = `<span class="${surplusOrDeficit.toLowerCase()}">${Math.abs(remainingCalories)} Calorie ${surplusOrDeficit}</span>
+                      <hr>
+                      <p>${budgetCalories} Calories Budgeted</p>
+                      <p>${consumedCalories} Calories Consumed</p>
+                      <p>${exerciseCalories} Calories burned</p>
+                     `;
+  output.classList.remove('hide');
 }
