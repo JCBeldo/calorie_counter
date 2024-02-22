f4374896785be74dbda352f4bae5a6403f105c7b
Julian Beldotti <julian.beldotti@gmail.com>
2024-02-21 21:30:17 -0700

    Fix: missing budgetCalories variable

--- script.js
+++ script.js
@@ -73,6 +73,7 @@ function calculateCalories(e) {
   const dinnerCalories = getCaloriesFromInputs(dinnerNumberInputs);
   const snacksCalories = getCaloriesFromInputs(snacksNumberInputs);
   const exerciseCalories = getCaloriesFromInputs(exerciseNumberInputs);
+  const budgetCalories = getCaloriesFromInputs(budgetInput);
 
   if (isError) {
     return;
